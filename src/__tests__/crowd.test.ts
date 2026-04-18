32f41d1ce03972f5bd33c8c16d791a5c474379dc
Narke Rushikesh Ashruba <hrushikeshnarake@gmail.com>
2026-04-18 20:53:06 +0530

    Update crowd.test.ts

--- src/__tests__/crowd.test.ts
+++ src/__tests__/crowd.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { getCrowdLevel } from "../utils/stadium-data";
+import { getCrowdLevel } from "../lib/stadium-data";
 
 describe("Crowd System", () => {
   it("should return a valid crowd level", () => {
