1121ac933537fd6296788d3a8e065bfb5320f270
Narke Rushikesh Ashruba <hrushikeshnarake@gmail.com>
2026-04-18 20:52:51 +0530

    Update alerts.test.ts

--- src/__tests__/alerts.test.ts
+++ src/__tests__/alerts.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { generateAlerts } from "../utils/stadium-data";
+import { generateAlerts } from "../lib/stadium-data";
 
 describe("Alerts System", () => {
   it("should return an array", () => {
