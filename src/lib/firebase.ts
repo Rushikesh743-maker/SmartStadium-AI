762dbcdd2c5e025e8229716957c7823f8078e535
Narke Rushikesh Ashruba <hrushikeshnarake@gmail.com>
2026-04-19 14:52:31 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,10 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyC2RvorRGHMmLGTdT1vvEpWOWwZMAqPJ14",
   authDomain: "smartstadium-ai-58b4a.firebaseapp.com",
