5e91c72cb997d802efd7a12139b349bd2dd682ca
Narke Rushikesh Ashruba <hrushikeshnarake@gmail.com>
2026-04-19 14:02:14 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,21 +12,39 @@ import { db } from "@/lib/firebase";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <ThemeProvider>
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </QueryClientProvider>
-  </ThemeProvider>
-);
+const App = () => {
+
+  useEffect(() => {
+    const initFirebase = async () => {
+      try {
+        await setDoc(doc(db, "logs", "init"), {
+          status: "firebase connected",
+          time: Date.now(),
+        });
+      } catch (err) {
+        console.log("Firebase error:", err);
+      }
+    };
+
+    initFirebase();
+  }, []);
+
+  return (
+    <ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </QueryClientProvider>
+    </ThemeProvider>
+  );
+};
 
 export default App;
