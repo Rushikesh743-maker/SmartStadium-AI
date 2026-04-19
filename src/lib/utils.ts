66c5c5ce11d558df48e7d54e8caf3844e3e3f55b
Narke Rushikesh Ashruba <hrushikeshnarake@gmail.com>
2026-04-19 15:08:56 +0530

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+export function sanitizeText(input: string): string {
+  if (!input) return "";
+
+  return input
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
